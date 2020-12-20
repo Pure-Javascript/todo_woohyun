21da14701b910b3ee34ed3181104b40e60556687
wkddngus5 <wkddngus5@naver.com>
2020-12-20 21:49:51 +0900

    feat: saving Todo

--- index.js
+++ index.js
@@ -1,26 +1,137 @@
-class Todo {
-  title;
-  content;
-  createdAt;
-  isDone;
-  
-  constructor({ title, content = '', createdAt = new Date(), isDone = false }) {
-    if (typeof title === 'undefined') {
-      new Error('Title is required');
-    }
-    this.title = title;
-    this.content = content;
-    this.createdAt = createdAt;
-    this.isDone = isDone;
-  }
-
-  toggleDone() {
-    this.isDone = !this.isDone;
-  }
-}
-
-
 window.onload = () => {
-  console.log('hi', Todo);
+  const LOCAL_STORAGE_KEY = 'todo-list';
+  const COLORS = {
+    blue: '#007bff',
+    indigo: '#6610f2',
+    purple: '#6f42c1',
+    pink: '#e83e8c',
+    red: '#dc3545',
+    orange: '#fd7e14',
+    yellow: '#ffc107',
+    green: '#28a745',
+    teal: '#20c997',
+    cyan: '#17a2b8',
+  };
 
+  // Classes
+  class TodoList {
+    list = [];
+
+    constructor({ list }) {
+      this.list = list;
+    }
+
+    add({ todo }) {
+      if ( !todo || !todo.title ) {
+        return;
+      }
+      this.list.push(todo);
+    }
+
+    toggleTodoDone({ index }) {
+      const targetTodo = this.list[index];
+      if (!targetTodo ) {
+        return;
+      }
+      targetTodo.toggleDone();
+    }
+  }
+
+  class Todo {
+    title;
+    content;
+    createdAt;
+    isDone;
+    
+    constructor({ title, content = '', createdAt = new Date(), isDone = false }) {
+      if (typeof title === 'undefined') {
+        new Error('Title is required');
+      }
+      this.title = title;
+      this.content = content;
+      this.createdAt = createdAt;
+      this.isDone = isDone;
+    }
+
+    toggleDone() {
+      this.isDone = !this.isDone;
+    }
+  }
+
+
+  // functions
+  const checkFormValidation = ({ data: { title = '' }}) => {
+    if (title.length === 0) {
+      return {key: 'title', message: 'title is required'};
+    }
+    return {};
+  }
+
+  const getRandomColor = () => Object.keys(COLORS)[Math.floor(Math.random(0, 12) * 10)];
+
+  
+  // Dom elements
+  const $app = document.querySelector('.todo-app');
+  const $form = $app.querySelector('.todo-app__form');
+  const $todoList = $app.querySelector('.todo-app__list');
+
+
+
+  // Initialize
+  const storageList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+  const todoList = new TodoList({ list: storageList });
+  const todoListDomString = todoList.list
+    .filter((todo) => todo && todo.title)
+    .map(({ title, content, createdAt, isDone }) => `
+      <li class="card list__item">
+        <span class="card__label ${getRandomColor()}"></span>
+        <h5>
+          ${title}
+        </h5>
+        <h6>
+          ${content}
+        </h6>
+      </li>
+    `)
+    .join('');
+  $todoList.insertAdjacentHTML('beforeend', todoListDomString);
+
+
+
+  // Event
+  $form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { currentTarget } = event;
+    const $items = currentTarget.querySelectorAll('.form__item');
+    const formEntries = Array.prototype.map.call($items, ($item) => {
+      const {dataset: { key }} = $item;
+      const {value = ''} = $item.querySelector('input, select, textarea');
+      return [key, value];
+    });
+    const formData = Object.fromEntries(formEntries);
+    const {key, message} = checkFormValidation({ data: formData });
+    if (key) {
+      const $invalidItem = currentTarget.querySelector(`.form__item[data-key=${key}]`);
+      $invalidItem.insertAdjacentHTML('beforeEnd', `
+        <span class="form__error">
+          ${message}
+        </span>
+      `);
+      return;
+    }
+    const newTodo = new Todo(formData);
+    todoList.add({ todo: newTodo });
+    localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      JSON.stringify(todoList.list),
+    );
+    $todoList.insertAdjacentHTML(
+      'beforeend',
+      `<li class="card">
+        <h5>${newTodo.title}</h5>
+      </li>`
+    );
+    
+    console.log(todoList);
+  });
 }
